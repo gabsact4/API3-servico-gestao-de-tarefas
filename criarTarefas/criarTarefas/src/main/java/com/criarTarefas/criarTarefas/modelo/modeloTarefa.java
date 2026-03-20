5d93b45215da6df31bc59bd4042526df32b0addb
Snake Eater <155785922+gabsact4@users.noreply.github.com>
2026-03-19 23:03:17 -0300

    feat: (#50) criar entidade/modelo Tarefa

---
+++ criarTarefas/criarTarefas/src/main/java/com/criarTarefas/criarTarefas/modelo/modeloTarefa.java
@@ -0,0 +1,89 @@
+package com.criarTarefas.criarTarefas.modelo;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class modeloTarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "tarefa", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<modeloItem> itens;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private String responsavel;
+
+    @Column(nullable = false)
+    private String prazo;
+
+    @Column(nullable = false)
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<modeloItem> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<modeloItem> itens) {
+        this.itens = itens;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public void setResponsavel(String responsavel) {
+        this.responsavel = responsavel;
+    }
+
+    public String getPrazo() {
+        return prazo;
+    }
+
+    public void setPrazo(String prazo) {
+        this.prazo = prazo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
