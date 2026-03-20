52cb4efa53479624fe87b085bfa62fcbe75d6392
Snake Eater <155785922+gabsact4@users.noreply.github.com>
2026-03-19 23:02:33 -0300

    feat: (#49) criar entidade/modelo Item

---
+++ criarTarefas/criarTarefas/src/main/java/com/criarTarefas/criarTarefas/modelo/modeloItem.java
@@ -0,0 +1,54 @@
+package com.criarTarefas.criarTarefas.modelo;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class modeloItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "tarefa_id", nullable = false)
+    @JsonIgnore
+    private modeloTarefa tarefa;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public modeloTarefa getTarefa() {
+        return tarefa;
+    }
+
+    public void setTarefa(modeloTarefa tarefa) {
+        this.tarefa = tarefa;
+    }
+}
